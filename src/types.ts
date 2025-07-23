8f1bd50b2dc019864fa9be29279dd9b6e445219c
Steve Hall <sh41@users.noreply.github.com>
2025-07-23 14:22:53 +0100

    ✨ Add turbopack support.
    New option available on configuration.
    
    example:
    ```
      services: [["next", { port: 3333, turbopack: true }]],
    ```

--- src/types.ts
+++ src/types.ts
@@ -14,4 +14,9 @@ export interface NextServiceOptions {
      * @default process.env.NUXT_PORT || config.devServer.port
      */
     port?: number
+    /**
+     * Whether to use the new turbopack  https://nextjs.org/docs/app/api-reference/turbopack incremental bundler
+     * @default false
+     */
+    turbopack?: boolean
 }
